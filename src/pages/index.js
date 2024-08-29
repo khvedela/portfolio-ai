4a984298e9bc1801080018fbd62d73f4383d9f78
David Khvedelidze <d.khv08@gmail.com>
2024-08-29 23:23:02 +0400

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,118 +1,99 @@
-import Image from "next/image";
 import { Inter } from "next/font/google";
+import Image from 'next/image'
+import { Linkedin, Mail, Phone } from 'lucide-react'
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
+    <div className="min-h-screen bg-gray-100 py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-4xl mx-auto bg-white shadow-xl rounded-lg overflow-hidden">
+        <div className="md:flex">
+          <div className="md:flex-shrink-0">
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+              className="h-48 w-full object-cover md:w-48"
+              src="/me.jpeg"
+              alt="Profile"
+              width={192}
+              height={192}
             />
-          </a>
+          </div>
+          <div className="p-8">
+            <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Web developer</div>
+            <h1 className="mt-2 text-3xl leading-8 font-bold tracking-tight text-gray-900 sm:text-4xl">David Khvedelidze</h1>
+            <p className="mt-2 text-gray-500">
+              A passionate Frontend Web Developer with Expertise in Modern Web Technologies and Frameworks.
+            </p>
+            <div className="mt-4 flex items-center">
+              <Mail className="h-5 w-5 text-gray-400" />
+              <a href="mailto:davidkhvedelidze@gmail.com" className="ml-2 text-gray-500 hover:text-gray-700">
+                davidkhvedelidze@gmail.com
+              </a>
+            </div>
+            <div className="mt-2 flex items-center">
+              <Linkedin className="h-5 w-5 text-gray-400" />
+              <a href="https://linkedin.com/in/khvedelidzedavid/" className="ml-2 text-gray-500 hover:text-gray-700">
+                david khvedelidze
+              </a>
+            </div>
+            <div className="mt-2 flex items-center">
+              <Phone className="h-5 w-5 text-gray-400" />
+              <span className="ml-2 text-gray-500">+995 511 300 322</span>
+            </div>
+          </div>
+        </div>
+        
+        <div className="px-8 py-6 border-t border-gray-200">
+          <h2 className="text-2xl font-bold text-gray-900">Experience</h2>
+          <div className="mt-4">
+            <h3 className="text-lg font-semibold text-gray-900">Angular developer</h3>
+            <p className="text-gray-500">TBC Bank • Sep. 2023 - Currently</p>
+            <ul className="mt-2 list-disc list-inside text-gray-600">
+              <li>Maintain multiple projects and ensure their functionality and usability.</li>
+              <li>Develop new features for projects using unit testing tools such as Jest and Spectator.</li>
+              <li>Implement NGRX state management for efficient data flow and organization in projects.</li>
+              <li>Utilize RxJS to create reactive applications and enhance user experience.</li>
+              <li>Structure complex projects to optimize performance and maintainability.</li>
+              <li>Collaborate with team members to ensure successful project delivery.</li>
+              <li>Stay up-to-date with the latest technologies and industry trends to continuously improve project quality and efficiency.</li>
+              <li>Proactively identify and resolve technical issues and challenges to maintain project progress.</li>
+              <li>Maintain thorough documentation and version control to ensure project stability and continuity.</li>
+              <li>Mentor junior developers and provide technical guidance and support as needed.</li>
+            </ul>
+          </div>
+        </div>
+        
+        <div className="px-8 pt-6 border-t border-gray-200">
+          <h2 className="text-2xl font-bold text-gray-900">Education</h2>
+          <div className="mt-4">
+            <h3 className="text-lg font-semibold text-gray-900">Business and Technology University</h3>
+            <p className="text-gray-500">Information Technologies • 2020 - 2024</p>
+          </div>
+        </div>
+
+        <div className="px-8 pb-6 border-gray-200">
+          <div className="mt-4">
+            <h3 className="text-lg font-semibold text-gray-900">Conservatoire National des Arts et Métiers</h3>
+            <p className="text-gray-500">Master of Science - MS, Computer Networks and IoT Systems • 2024 - current</p>
+          </div>
+        </div>
+        
+        <div className="px-8 py-6 border-t border-gray-200">
+          <h2 className="text-2xl font-bold text-gray-900">Languages</h2>
+          <ul className="mt-2 list-disc list-inside text-gray-600">
+            <li>Georgian (Native)</li>
+            <li>English (Fluent)</li>
+            <li>Russian (Fluent)</li>
+          </ul>
+        </div>
+        
+        <div className="px-8 py-6 border-t border-gray-200">
+          <h2 className="text-2xl font-bold text-gray-900">Skills</h2>
+          <p className="mt-2 text-gray-600">
+            Javascript - Angular - HTML - CSS - SCSS - Git - Github - Webpack - Styled Components - NGRX - RXJS - UI design - Figma
+          </p>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+    </div>
+  )
 }
